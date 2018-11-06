7af5ec3ab02f03f4b548fd133f5e2a4ea83edcfe
Jim Jack <jim_jack@Jims-MacBook-Pro.local>
2018-11-06 17:14:16 -0500

    song,artist,genre complex sqlalch joins

--- models.py
+++ models.py
@@ -4,11 +4,28 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
-# Write your classes below
+class Artist(Base):
+    __tablename__ = 'artists'
+    id=Column(Integer,primary_key=True)
+    name=Column(Text)
+    songs = relationship('Song', back_populates='artist')
+    genres = relationship('Genre', secondary='songs', back_populates='artists')
 
+class Genre(Base):
+    __tablename__ = 'genres'
+    id=Column(Integer,primary_key=True)
+    name=Column(Text)
+    songs = relationship('Song', back_populates='genre')
+    artists = relationship('Artist', secondary='songs', back_populates='genres')
 
-
-
+class Song(Base):
+    __tablename__ = 'songs'
+    id=Column(Integer,primary_key=True)
+    name=Column(Text)
+    artist_id = Column(Integer, ForeignKey('artists.id'))#, primary_key=True)
+    genre_id = Column(Integer, ForeignKey('genres.id'))#, primary_key=True)
+    artist = relationship('Artist', back_populates='songs')
+    genre = relationship('Genre', back_populates='songs')
 
 
 engine = create_engine('sqlite:///:memory:')
--- queries.py
+++ queries.py
@@ -1,5 +1,10 @@
-from models import *
+from seed import *
 
-def query_experimentation(session):
+def query_experimentation():
     pass
     # import pdb; pdb.set_trace()
+def grateful_dead_genres():
+    return session.query(Genre.name).join(Song).join(Artist).filter(Artist.name == "Grateful Dead").all()
+
+def all_grateful_dead_songs():
+    return session.query(Song.name).join(Artist).filter(Artist.name == "Grateful Dead").all()
